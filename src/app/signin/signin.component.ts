302dfec3ffc4910313a4e1f690aa19f8b863fd92
pao407 <paoberu407@gmail.com>
2024-10-21 18:12:49 -0500

    ya se visualisa la foto del perfil en registrar

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'; // Importa el Router para la navegación
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [CommonModule,HeaderComponent, FooterComponent],
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
   profilePictureUrl: string | ArrayBuffer | null = null;
 
-  constructor(private router: Router) {}
+  constructor(private readonly router: Router) {}
 
   // Método para manejar el registro
   onSubmit() {
@@ -24,7 +25,7 @@ export class SigninComponent {
   // Método para manejar la selección de archivos
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
+    if (input.files?.[0]) {
       const file = input.files[0];
       const reader = new FileReader();
       reader.onload = () => {
