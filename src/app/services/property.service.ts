1310ad1a0581fee276b56f177615402c269620f4
{bananna2} <{anasofiamr1@hotmail.com}>
2024-10-24 12:38:02 -0500

    crear propiedad sirviendo

--- src/app/services/property.service.ts
+++ src/app/services/property.service.ts
@@ -3,25 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PropertyService {
-  private apiUrl = 'http://localhost:8080/property';  // URL de tu backend
+  private apiUrl = 'http://localhost:8080/property'; 
 
   constructor(private http: HttpClient) {}
 
-  // Método para crear una propiedad usando HttpClient
   createProperty(property: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, property);
-  }
-
-  // Obtener una propiedad por su ID
-  getPropertyById(id: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${id}`);
-  }
-
-  // Actualizar una propiedad
-  updateProperty(property: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${property.id}`, property);
+    return this.http.post<any>(this.apiUrl, property);
   }
 }
