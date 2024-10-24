0d45ee127412e696062d70f1b6119e772ecd059c
richie <richiegut30@gmail.com>
2024-10-23 23:42:13 -0500

    Componente Pago

--- src/app/components/pago/pago.component.ts
+++ src/app/components/pago/pago.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pago',
-  standalone: true,
-  imports: [],
   templateUrl: './pago.component.html',
-  styleUrl: './pago.component.css'
+  styleUrls: ['./pago.component.css']
 })
 export class PagoComponent {
+  pagoForm: FormGroup;
+  bancos = ['Banco A', 'Banco B', 'Banco C', 'Banco D'];
 
+  constructor(private fb: FormBuilder) {
+    this.pagoForm = this.fb.group({
+      valor: [{ value: 1000000, disabled: true }, Validators.required],
+      banco: ['', Validators.required],
+      numeroCuenta: ['', [Validators.required, Validators.pattern('^[0-9]*$')]]
+    });
+  }
+
+  onSubmit() {
+    if (this.pagoForm.valid) {
+      alert('Pago realizado con éxito');
+    }
+  }
 }
