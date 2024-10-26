b4a721794932cbe93eb3d3c397aa1d267572ee67
pao407 <paoberu407@gmail.com>
2024-10-25 22:01:28 -0500

    pago y solicitudes hecho :D

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component'; // Importa el componente de registro
 import { Home2Component } from './home2/home2.component';
+import { PayComponent } from './pay/pay.component';
+import { SolicitudesComponent } from './solicitudes/solicitudes.component';
+
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '',  component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signin', component: SigninComponent }, // Ruta para el registro
-  { path: 'home2', component: Home2Component }
+  { path: 'home2', component: Home2Component },
+  { path: 'pay', component: PayComponent },
+  { path: 'solicitudes', component: SolicitudesComponent }
 ];
