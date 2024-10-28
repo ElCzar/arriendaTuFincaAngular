bb4122a1cd1ae2af86fde4a970b9d5cc2e21dd72
pao407 <paoberu407@gmail.com>
2024-10-27 19:28:24 -0500

    barra de busqueda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { HearderAuthComponent } from "./components/hearder-auth/hearder-auth.component";
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, HearderAuthComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HearderAuthComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/home2/home2.component.ts
+++ src/app/components/home2/home2.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HearderAuthComponent } from '../hearder-auth/hearder-auth.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-home2',
   standalone: true,
-  imports: [FooterComponent, HearderAuthComponent],
+  imports: [FooterComponent, HearderAuthComponent, SearchBarComponent],
   templateUrl: './home2.component.html',
   styleUrls: ['./home2.component.css'] // Corregido: styleUrls en lugar de styleUrl
 })
